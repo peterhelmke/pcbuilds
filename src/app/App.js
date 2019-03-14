bff978432ba34307b88b11174cc1f3ec34499d42
Peter Helmke <peteybxt@googlemail.com>
2019-03-14 14:17:23 +0100

    Cards with 2 pages and prices

--- src/app/App.js
+++ src/app/App.js
@@ -3,8 +3,9 @@ import styled from 'styled-components'
 import CardContainer from '../cards/CardContainer'
 import GlobalStyle from './GlobalStyle'
 import CardsRender from '../cards/CardsRender'
-import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
-import { FaStar, FaFilter, FaSort } from 'react-icons/fa'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from '../common/Header'
+import Footer from '../common/Footer'
 
 const PageGrid = styled.div`
   max-width: 1025px;
@@ -19,194 +20,299 @@ const PageGrid = styled.div`
   width: 100%;
 `
 
-const StyledHeader = styled.header`
-  display:grid;
-  grid-template-rows: 50px 50px;
-  background: linear-gradient(to right, #11002C, #330086);
-`
-
-const HeaderTitleContainer = styled(NavLink)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  text-decoration: none;
-`
-const HeaderTitleWhite = styled.div`
-  font-family: 'Oswald', sans-serif;
-  font-size: 28px;
-  color: #F1F1F1;
-`
-
-const HeaderTitleYellow = styled.div`
-  font-family: 'Oswald', sans-serif;
-  font-size: 28px;
-  color: #FFDE00;
-`
-
-const HeaderCategoryNav = styled.nav`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  overflow-x: scroll;
-  scroll-snap-type: x mandatory;
-  -webkit-overflow-scrolling: touch;
-`
-
-const HeaderCategoryLink = styled(NavLink)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 24px;
-  font-family: 'Oswald', sans-serif;
-  text-decoration: none;
-  color: rgba(255,255,255,0.5);
-  background: rgba(192,192,192,0.12);
-  &.active {
-    background: transparent;
-    color: #F1F1F1;
-  }
-`
-
-const FooterNav = styled.nav`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  height: 50px;
-  background: linear-gradient(to right, #11002C, #330086);
-`
-
-const FooterNavLink = styled(NavLink)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: rgba(255,255,255,0.5);
-  font-size: 22px;
-`
-
-const FooterNavBookmark = styled(NavLink)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: rgba(255,255,255,0.5);
-  font-size: 25px;
-  &.active {
-    color: #F1F1F1;
-  }
-`
-
 function App() {
   const [cards, setCards] = useState([
-    {title: 'Apex Gaming Build',
-    subtitle: 'Featured Build',
-    image: 'images/phanteksp300tgbk.jpg',
-    manufactureritem1: 'AMD',
-    item1: 'Ryzen 5 2600X',
-    manufactureritem2: 'nvidia',
-    item2: 'GeForce RTX 2060',
-    manufactureritem3: 'Phanteks',
-    item3: 'Eclipse P300 Tempered Glass',
-    manufactureritem4: 'GeIL',
-    item4: 'EVO SPEAR 16 GB (2 x 8 GB)',
-    total: '943.32',
-    category: 'gaming',
-    bookmarked: false},
+    {
+      title: 'Apex Gaming Build',
+      subtitle: 'Featured Build',
+      image: 'images/phanteksp300tgbk.jpg',
+      keyFacts:
+        'A build capable of all modern titles up to WQHD-resolution and simultanous streaming.',
+      parts: [
+        {
+          partManufacturer: 'AMD',
+          partName: 'Ryzen 5 2600X',
+          partPrice: 196.89,
+          partUrl: 'https://amzn.to/2EXQxDI',
+          partAsin: 'asin.B07B428V2L',
+        },
+        {
+          partManufacturer: 'MSI',
+          partName: 'GeForce RTX 2060 6GB Ventus OC',
+          partPrice: 374.99,
+          partUrl: 'https://amzn.to/2Tzb5gc',
+          partAsin: 'asin.B07MJC8XKD',
+        },
+        {
+          partManufacturer: 'Phanteks',
+          partName: 'Eclipse P300 Tempered Glass',
+          partPrice: 62.94,
+          partUrl: 'https://amzn.to/2Cgb4TO',
+          partAsin: 'asin.B075DMYVBV',
+        },
+        {
+          partManufacturer: 'Ballistix',
+          partName: 'Sport LT BLS2K8G4D30BESBK 16GB (8GB x2)',
+          partPrice: 100.68,
+          partUrl: 'https://amzn.to/2TBcmmL',
+          partAsin: 'asin.B07HP87R9J',
+        },
+        {
+          partManufacturer: 'ASRock',
+          partName: 'B450 Pro4',
+          partPrice: 82.99,
+          partUrl: 'https://amzn.to/2CdKz0X',
+          partAsin: 'asin.B07FVYKJHR',
+        },
+        {
+          partManufacturer: 'Cooler Master',
+          partName: 'Hyper 212 Black',
+          partPrice: 34.94,
+          partUrl: 'https://amzn.to/2Cg215a',
+          partAsin: 'asin.B07H9HJLG5',
+        },
+        {
+          partManufacturer: 'SanDisk',
+          partName: 'SSD PLUS 1TB',
+          partPrice: 112.9,
+          partUrl: 'https://amzn.to/2XWlFMB',
+          partAsin: 'asin.B07D998212',
+        },
+        {
+          partManufacturer: 'Corsair',
+          partName: 'TXM Gold 550W 80+ Gold',
+          partPrice: 77.99,
+          partUrl: 'https://amzn.to/2O1rxjf',
+          partAsin: 'asin.B06WV7Z1ZW',
+        },
+      ],
+      total: 2,
+      category: 'gaming',
+      bookmarked: false,
+    },
 
-    {title: 'AMD Workstation Build',
-    subtitle: 'Edit like a Pro',
-    image: 'images/fractaldesignmeshifyctg.jpg',
-    manufactureritem1: 'AMD',
-    item1: 'Ryzen 7 2700',
-    manufactureritem2: 'AMD',
-    item2: 'Radeon Pro WX 8200',
-    manufactureritem3: 'Fractal Design',
-    item3: 'Meshify C Tempred Glass',
-    manufactureritem4: 'G.Skill',
-    item4: 'Aegis 32 GB (4 x 8 GB)',
-    total: '1677.43',
-    category: 'editing',
-    bookmarked: false},
+    {
+      title: 'AMD Workstation Build',
+      subtitle: 'Edit like a Pro!',
+      image: 'images/fractaldesignmeshifyctg.jpg',
+      keyFacts: 'Commander Eilo am Start!',
+      parts: [
+        {
+          partManufacturer: 'AMD',
+          partName: 'Ryzen 5 2600X',
+          partPrice: 196.89,
+          partUrl: 'https://amzn.to/2EXQxDI',
+          partAsin: 'asin.B07B428V2L',
+        },
+        {
+          partManufacturer: 'MSI',
+          partName: 'GeForce RTX 2060 6GB Ventus OC',
+          partPrice: 374.99,
+          partUrl: 'https://amzn.to/2Tzb5gc',
+          partAsin: 'asin.B07MJC8XKD',
+        },
+        {
+          partManufacturer: 'Fractal Design',
+          partName: 'Meshify C Tempered Glass',
+          partPrice: 92.58,
+          partUrl: 'https://amzn.to/2Tx4Edz',
+          partAsin: 'asin.B079FQZM89',
+        },
+        {
+          partManufacturer: 'Ballistix',
+          partName: 'Sport LT BLS2K8G4D30BESBK 16GB (8GB x2)',
+          partPrice: 100.68,
+          partUrl: 'https://amzn.to/2TBcmmL',
+          partAsin: 'asin.B07HP87R9J',
+        },
+        {
+          partManufacturer: 'ASRock',
+          partName: 'B450 Pro4',
+          partPrice: 82.99,
+          partUrl: 'https://amzn.to/2CdKz0X',
+          partAsin: 'asin.B07FVYKJHR',
+        },
+        {
+          partManufacturer: 'Cooler Master',
+          partName: 'Hyper 212 Black',
+          partPrice: 34.94,
+          partUrl: 'https://amzn.to/2Cg215a',
+          partAsin: 'asin.B07H9HJLG5',
+        },
+        {
+          partManufacturer: 'SanDisk',
+          partName: 'SSD PLUS 1TB',
+          partPrice: 112.9,
+          partUrl: 'https://amzn.to/2XWlFMB',
+          partAsin: 'asin.B07D998212',
+        },
+        {
+          partManufacturer: 'Corsair',
+          partName: 'TXM Gold 550W 80+ Gold',
+          partPrice: 77.99,
+          partUrl: 'https://amzn.to/2O1rxjf',
+          partAsin: 'asin.B06WV7Z1ZW',
+        },
+      ],
+      total: 3,
+      category: 'editing',
+      bookmarked: false,
+    },
 
-    {title: 'Budget Office Build',
-    subtitle: 'Bargain Build',
-    image: 'images/CoolerMasterMasterBoxE300L.jpg',
-    manufactureritem1: 'AMD',
-    item1: 'Athlon 200GE',
-    manufactureritem2: 'TCSunBow',
-    item2: 'X3 480 GB',
-    manufactureritem3: 'Cooler Master',
-    item3: 'MasterBox E300L',
-    manufactureritem4: 'G.Skill',
-    item4: 'Ripjaws V Series 8 GB (2 x 4 GB)',
-    total: '277.85',
-    category: 'office',
-    bookmarked: false},
+    {
+      title: 'Office PC',
+      subtitle: 'Bargain Build',
+      image: 'images/CoolerMasterMasterBoxE300L.jpg',
+      keyFacts: 'Office SWAG',
+      parts: [
+        {
+          partManufacturer: 'AMD',
+          partName: 'Ryzen 5 2600X',
+          partPrice: 196.89,
+          partUrl: 'https://amzn.to/2EXQxDI',
+          partAsin: 'asin.B07B428V2L',
+        },
+        {
+          partManufacturer: 'MSI',
+          partName: 'GeForce RTX 2060 6GB Ventus OC',
+          partPrice: 374.99,
+          partUrl: 'https://amzn.to/2Tzb5gc',
+          partAsin: 'asin.B07MJC8XKD',
+        },
+        {
+          partManufacturer: 'Cooler Master',
+          partName: 'Master Box E300L',
+          partPrice: 34.99,
+          partUrl: 'https://amzn.to/2TyB3QR',
+          partAsin: 'asin.B07C2KNP2Q',
+        },
+        {
+          partManufacturer: 'Ballistix',
+          partName: 'Sport LT BLS2K8G4D30BESBK 16GB (8GB x2)',
+          partPrice: 100.68,
+          partUrl: 'https://amzn.to/2TBcmmL',
+          partAsin: 'asin.B07HP87R9J',
+        },
+        {
+          partManufacturer: 'ASRock',
+          partName: 'B450 Pro4',
+          partPrice: 82.99,
+          partUrl: 'https://amzn.to/2CdKz0X',
+          partAsin: 'asin.B07FVYKJHR',
+        },
+        {
+          partManufacturer: 'Cooler Master',
+          partName: 'Hyper 212 Black',
+          partPrice: 34.94,
+          partUrl: 'https://amzn.to/2Cg215a',
+          partAsin: 'asin.B07H9HJLG5',
+        },
+        {
+          partManufacturer: 'SanDisk',
+          partName: 'SSD PLUS 1TB',
+          partPrice: 112.9,
+          partUrl: 'https://amzn.to/2XWlFMB',
+          partAsin: 'asin.B07D998212',
+        },
+        {
+          partManufacturer: 'Corsair',
+          partName: 'TXM Gold 550W 80+ Gold',
+          partPrice: 77.99,
+          partUrl: 'https://amzn.to/2O1rxjf',
+          partAsin: 'asin.B06WV7Z1ZW',
+        },
+      ],
+      total: 1,
+      category: 'office',
+      bookmarked: false,
+    },
   ])
 
-  function bookmarkToggle(title) {
-    const card = cards.find(pcbuild => pcbuild.title === title)
+  function bookmarkToggle(id) {
+    const card = cards.find(pcbuild => pcbuild.id === id)
     const index = cards.indexOf(card)
-    setCards([...cards.slice(0, index), {...card, bookmarked: !card.bookmarked}, ...cards.slice(index + 1)])
+    setCards([
+      ...cards.slice(0, index),
+      { ...card, bookmarked: !card.bookmarked },
+      ...cards.slice(index + 1),
+    ])
   }
 
   return (
-<Router>
-  <PageGrid>
-    <StyledHeader>
-      <HeaderTitleContainer exact to="/">
-        <HeaderTitleWhite>build</HeaderTitleWhite>
-        <HeaderTitleYellow>IT</HeaderTitleYellow>
-      </HeaderTitleContainer>
-      <HeaderCategoryNav>
-        <HeaderCategoryLink exact to="/">All</HeaderCategoryLink>
-        <HeaderCategoryLink to="/gaming">Gaming</HeaderCategoryLink>
-        <HeaderCategoryLink to="/editing">Editing</HeaderCategoryLink>
-        <HeaderCategoryLink to="/office">Office</HeaderCategoryLink>
-      </HeaderCategoryNav>
-    </StyledHeader>
-    <CardContainer>
-      <Route
-        exact path="/"
-        render={() => ( <CardsRender cards={cards} bookmarkToggle={bookmarkToggle} />)}
-      />
-      <Route
-        path="/gaming"
-        render={() => (
-          <CardsRender 
-            bookmarkToggle={bookmarkToggle} 
-            cards={cards.filter(card => card.category === 'gaming')} />
-        )}
-      />
-      <Route
-        path="/editing"
-        render={() => (
-          <CardsRender 
-            bookmarkToggle={bookmarkToggle} 
-            cards={cards.filter(card => card.category === 'editing')} />
-        )}
-      />
-      <Route
-        path="/office"
-        render={() => (
-          <CardsRender 
-            bookmarkToggle={bookmarkToggle} 
-            cards={cards.filter(card => card.category === 'office')} />
-        )}
-      />
-      <Route
-        path="/bookmarks"
-        render={() => (
-          <CardsRender 
-            bookmarkToggle={bookmarkToggle} 
-            cards={cards.filter(card => card.bookmarked)} />
-        )}
-      />
-    </CardContainer>
-    <FooterNav>
-      <FooterNavLink to="/"><FaSort /></FooterNavLink>
-      <FooterNavLink to="/"><FaFilter /></FooterNavLink>
-      <FooterNavBookmark to="/bookmarks"><FaStar /></FooterNavBookmark>
-    </FooterNav>
-    <GlobalStyle />
-  </PageGrid>
-</Router>
+    <Router>
+      <PageGrid>
+        <Header />
+        <CardContainer>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <CardsRender bookmarkToggle={bookmarkToggle} cards={cards} />
+            )}
+          />
+          <Route
+            path="/gaming"
+            render={() => (
+              <CardsRender
+                bookmarkToggle={bookmarkToggle}
+                cards={cards.filter(card => card.category === 'gaming')}
+              />
+            )}
+          />
+          <Route
+            path="/editing"
+            render={() => (
+              <CardsRender
+                bookmarkToggle={bookmarkToggle}
+                cards={cards.filter(card => card.category === 'editing')}
+              />
+            )}
+          />
+          <Route
+            path="/office"
+            render={() => (
+              <CardsRender
+                bookmarkToggle={bookmarkToggle}
+                cards={cards.filter(card => card.category === 'office')}
+              />
+            )}
+          />
+          <Route
+            path="/ascending"
+            render={() => (
+              <CardsRender
+                bookmarkToggle={bookmarkToggle}
+                cards={cards.sort(
+                  (a, b) => parseFloat(a.total) - parseFloat(b.total)
+                )}
+              />
+            )}
+          />
+          <Route
+            path="/descending"
+            render={() => (
+              <CardsRender
+                bookmarkToggle={bookmarkToggle}
+                cards={cards.sort(
+                  (a, b) => parseFloat(b.total) - parseFloat(a.total)
+                )}
+              />
+            )}
+          />
+          <Route
+            path="/bookmarks"
+            render={() => (
+              <CardsRender
+                bookmarkToggle={bookmarkToggle}
+                cards={cards.filter(card => card.bookmarked)}
+              />
+            )}
+          />
+        </CardContainer>
+        <Footer />
+        <GlobalStyle />
+      </PageGrid>
+    </Router>
   )
 }
 
